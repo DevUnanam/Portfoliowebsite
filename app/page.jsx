a49fc0637566c0307117d3bbb8b105cce8cffd16
DevUnanam <riaahh20@yahoo.com>
2025-10-07 18:40:00 +0100

    Updated cv section and updated new cv file

--- app/page.jsx
+++ app/page.jsx
@@ -12,10 +12,10 @@ const Home = () => {
         <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">FULLSTACK SOFTWARE DEVELOPER</span>
           <h1 className="h1">
-            Hello I'm <br /> <span>Unanam Victoria</span>
+            Hello I'm <br /> <span>Victoria Joseph</span>
           </h1>
           <p className="max-w-[500px] mb-9 text-white/80">
-          I am a highly skilled developer with the ability to build and develop amazing applications.
+          I am a seasoned Django developer with over 3 years of experience in building robust web applications. My expertise lies in creating scalable backend systems, integrating APIs, and ensuring seamless user experiences. I am passionate about leveraging technology to solve real-world problems and I'm always eager to take on new challenges in the tech world.
           </p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
             {/* <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
@@ -23,8 +23,8 @@ const Home = () => {
               <FiDownload className="text-xl" />
             </Button> */}
             <a
-                href="/Victorria.pdf"
-                download="Victorria.pdf"
+                href="/Victoria Joseph Resume.pdf"
+                download="Victoria Joseph Resume.pdf"
                 className="uppercase flex items-center gap-2 border border-gray-500 text-gray-500 px-6 py-3 rounded-lg hover:bg-gray-700 hover:text-white transition"
               >
                 <span>Download CV</span>
